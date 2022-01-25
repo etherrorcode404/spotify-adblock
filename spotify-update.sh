29c19f7c6f11a8ee2c77c336b46c8290743a28a6
Ether <ahanafether@gmail.com>
2022-01-25 10:45:54 +0600

    Update spotify-update.sh

--- spotify-update.sh
+++ spotify-update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-rm -rf /usr/local/share/applications/spotify.desktop
-rm -rf spotify-adblock
+sudo rm -rf /usr/local/share/applications/spotify.desktop
+sudo rm -rf spotify-adblock
 git clone https://github.com/Ahanaf-Ether/spotify-adblock.git
 cd spotify-adblock
-mv ~/spotify-adblock/spotify.desktop /usr/local/share/applications/   
-chmod +x /usr/local/share/applications/spotify.desktop
+sudo mv ~/spotify-adblock/spotify.desktop /usr/local/share/applications/   
+sudo chmod +x /usr/local/share/applications/spotify.desktop
